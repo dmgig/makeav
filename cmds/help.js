d4c0e69ce759f37fd1f9748af94d074a2b620a59
Giglio, Dave <dgiglio@penguinrandomhouse.com>
2018-11-28 14:30:09 -0500

    clean up, format help display

--- cmds/help.js
+++ cmds/help.js
@@ -1,47 +1,57 @@
+const topCommon = `--orderid, -o ....... (string) order id, directory name [required]
+    --audiofile, -a ..... (string) audio filename [required]
+    --logo, -l .......... (string) logo image file`
+
+const lowCommon = `--wavevizcolor ...... (web color) wave visual, if false, wave will not appear
+    --wavevizmode ....... (lin|log|sqrt|cbrt) wave visual mode, defaults to 'lin'
+    --outname, -o ....... (string) output name, defaults to orderid-date`
+
 const menus = {
   main: `
     makeav [command] <options>
 
     init ............... create working, output directories
-    config ............... show current config values
+    config ............. show current config values
     resize ............. resize images
     slide .............. create a slide show
     static ............. create a show with a static image
     version ............ show package version
-    help ............... show help menu for a command`,
+    help ............... show help menu for a command
+`,
 
   init: `
-    makeav init`,
+    makeav init
+`,
 
   init: `
-    makeav init`,
+    makeav init
+`,
 
   resize: `
-    makeav resize`,
+    makeav resize
+`,
 
   slide: `
     makeav slide <options>
 
-    --orderid, -o ....... (string) order id, directory name [required]
-    --audiofile, -a ..... (string) audio filename [required]
-    --resize, -r ........ (bool) if set to true, will resize images to fit the slides and convert them to pngs, which is required if they are not all 960x640 pngs
+    ${topCommon}
+    --resize, -r ........ (bool) if set to true, will resize images to fit the
+                          slides and convert them to pngs, which is required if
+                          they are not all 960x640 pngs
     --duration, -d ...... (int) slide duration, defaults to 8
-    --wavevizcolor ...... (web color) wave visual, if false, wave will not appear
-    --wavevizmode ....... (lin|log|sqrt|cbrt) wave visual mode, defaults to 'lin'
-    --logo, -l .......... (string) logo image file
-    --outname, -o ....... (string) output name, defaults to orderid-date`,
+    ${lowCommon}
+`,
 
   static: `
     makeav slide <options>
 
-    --orderid, -o ....... (string) order id, directory name [required]
-    --audiofile, -a ..... (string) audio filename [required]
-    --resize, -r ........ (bool) if set to true, will resize images to fit the slides and convert them to pngs, which is required if they are not all 960x640 pngs
-    --wavevizcolor ...... (web color) wave visual, if false, wave will not appear
-    --wavevizmode ....... (lin|log|sqrt|cbrt) wave visual mode, defaults to 'lin'
-    --logo, -l .......... (string) logo image file
+    ${topCommon}
+    --resize, -r ........ (bool) if set to true, will resize the static image
+                          and convert them to pngs, which is required if it is
+                          not a 960x640 png
     --static, -s ........ (string) main image file
-    --outname, -o ....... (string) output name, defaults to orderid-date`,
+    ${lowCommon}
+`
 }
 
 module.exports = (args) => {

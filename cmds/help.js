8e358a48c0319c1eda9472d063e171c91f947028
Giglio, Dave <dgiglio@penguinrandomhouse.com>
2018-11-28 13:09:51 -0500

    add resize info to help

--- cmds/help.js
+++ cmds/help.js
@@ -24,8 +24,9 @@ const menus = {
 
     --orderid, -o ....... (string) order id, directory name [required]
     --audiofile, -a ..... (string) audio filename [required]
+    --resize, -r ........ (bool) if set to true, will resize images to fit the slides and convert them to pngs, which is required if they are not all 960x640 pngs
     --duration, -d ...... (int) slide duration, defaults to 8
-    --wavevizcolor ....... (web color) wave visual, if false, wave will not appear
+    --wavevizcolor ...... (web color) wave visual, if false, wave will not appear
     --wavevizmode ....... (lin|log|sqrt|cbrt) wave visual mode, defaults to 'lin'
     --logo, -l .......... (string) logo image file
     --outname, -o ....... (string) output name, defaults to orderid-date`,
@@ -35,10 +36,11 @@ const menus = {
 
     --orderid, -o ....... (string) order id, directory name [required]
     --audiofile, -a ..... (string) audio filename [required]
-    --wavevizcolor ....... (web color) wave visual, if false, wave will not appear
+    --resize, -r ........ (bool) if set to true, will resize images to fit the slides and convert them to pngs, which is required if they are not all 960x640 pngs
+    --wavevizcolor ...... (web color) wave visual, if false, wave will not appear
     --wavevizmode ....... (lin|log|sqrt|cbrt) wave visual mode, defaults to 'lin'
     --logo, -l .......... (string) logo image file
-    --static, -s ....... (string) main image file
+    --static, -s ........ (string) main image file
     --outname, -o ....... (string) output name, defaults to orderid-date`,
 }
 

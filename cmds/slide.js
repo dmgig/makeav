538d5dc20810f4aae572fd4d4c1c2467142194a3
Giglio, Dave <dgiglio@penguinrandomhouse.com>
2018-11-28 14:58:34 -0500

    whitespace

--- cmds/slide.js
+++ cmds/slide.js
@@ -10,19 +10,19 @@ const C = require('../constants')
 module.exports = async (args) => {
     console.log('makeav conf', args)
   try {
-    const runtime = moment().format('YYYYMMDDHHmmss');
-    const orderid = args.orderid || args.o
-    const audiofile = args.audiofile || args.a
-    const duration = args.duration || args.d || C.durationDefault
-    const wavevizmode = args.wavevizmode || 'line'
-    const wavevizcolor = args.wavevizcolor || false
-    const logofile = args.logo || args.l || C.logoFilename
-    const outname = `${(args.outname || args.o || orderid)}-${runtime}.mp4`
-    const outPath = `${conf.get('outputDirPath')}/${outname}`
-    const workingPath = conf.get('workingDirPath')
-    const orderPath = `${workingPath}/${orderid}`
+    const runtime       = moment().format('YYYYMMDDHHmmss');
+    const orderid       = args.orderid || args.o
+    const audiofile     = args.audiofile || args.a
+    const duration      = args.duration || args.d || C.durationDefault
+    const wavevizmode   = args.wavevizmode || 'line'
+    const wavevizcolor  = args.wavevizcolor || false
+    const logofile      = args.logo || args.l || C.logoFilename
+    const outname       = `${(args.outname || args.o || orderid)}-${runtime}.mp4`
+    const outPath       = `${conf.get('outputDirPath')}/${outname}`
+    const workingPath   = conf.get('workingDirPath')
+    const orderPath     = `${workingPath}/${orderid}`
     const slideshowPath = `${orderPath}/slideshow`
-    const resizedPath = `${orderPath}/${C.workFilesDir}`
+    const resizedPath   = `${orderPath}/${C.workFilesDir}`
 
     if(!orderid) error('ERROR: Order Id Required.')
     if(!audiofile) error('ERROR: Audiofile Required.')
--- cmds/static.js
+++ cmds/static.js
@@ -10,19 +10,19 @@ const C = require('../constants')
 module.exports = async (args) => {
 
   try {
-    const runtime = moment().format('YYYYMMDDHHmmss');
-    const orderid = args.orderid || args.o
-    const audiofile = args.audiofile || args.a
-    const wavevizmode = args.wavevizmode || 'line'
+    const runtime      = moment().format('YYYYMMDDHHmmss');
+    const orderid      = args.orderid || args.o
+    const audiofile    = args.audiofile || args.a
+    const wavevizmode  = args.wavevizmode || 'line'
     const wavevizcolor = args.wavevizcolor || false
-    const outname = `${(args.outname || args.o || orderid)}-${runtime}.mp4`
-    const outPath = `${conf.get('outputDirPath')}/${outname}`
-    const staticFile = args.static || args.s || false
-    const logoFile = args.logo || args.l || false
-    const workingPath = conf.get('workingDirPath')
-    const orderPath = `${workingPath}/${orderid}`
-    const resizedPath = `${orderPath}/${C.workFilesDir}`
-    const imageFiles = await files.getListOfImageFiles(resizedPath)
+    const outname      = `${(args.outname || args.o || orderid)}-${runtime}.mp4`
+    const outPath      = `${conf.get('outputDirPath')}/${outname}`
+    const staticFile   = args.static || args.s || false
+    const logoFile     = args.logo || args.l || false
+    const workingPath  = conf.get('workingDirPath')
+    const orderPath    = `${workingPath}/${orderid}`
+    const resizedPath  = `${orderPath}/${C.workFilesDir}`
+    const imageFiles   = await files.getListOfImageFiles(resizedPath)
 
     if(!orderid) error('ERROR: Order Id Required.', true)
     if(!audiofile) error('ERROR: Audiofile Required.', true)
